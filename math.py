c9f3fe155f090ba2003957ccd14ed63d92d8d925
Q2016 <k.sohraby@gmail.com>
2022-06-13 18:07:40 -0700

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,4 @@
 carry, A[i] = divmod(A[i], 10) # If x and y are integers, the return value from divmod() is same as (a // b, x % y)
+
+# enumerate all possibilities, with the permutation() func
+for h, i, j, k in itertools.permutations(A):
