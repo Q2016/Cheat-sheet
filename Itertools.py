8920480599905ab4c3be5fc0c56d8109c593a276
Q2016 <k.sohraby@gmail.com>
2022-06-14 11:48:15 -0700

    Update Itertools.py

--- Itertools.py
+++ Itertools.py
@@ -1,3 +1,12 @@
-// enumerate all possibilities, with the permutation() func
+# enumerate all possibilities, with the permutation() func
 A=[1,2,3,4]        
 for h, i, j, k in itertools.permutations(A):
+  
+  
+  
+people = iter(list1) # list1=[1,2,3]
+# or
+people = (i for i, seat in enumerate(seats) if seat)
+
+future = next(people)
+future = next(people, None) # If default is given, it is returned if the iterator is exhausted
