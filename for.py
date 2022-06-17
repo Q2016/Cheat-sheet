94d9258c3fb5e63cc641a3519dc2875b15384d9c
Q2016 <k.sohraby@gmail.com>
2022-06-17 16:23:31 -0700

    Update for.py

--- for.py
+++ for.py
@@ -1,2 +1,10 @@
 # if trust=[[1,2],[2,3]]
 for (a, b) in trust:
+
+ 
+def sub_lists(l,m): # maybe this can be replaced with itertools.permutation()
+    lists = [[]]
+    for i in range(len(l) + 1):
+        for j in range(i):
+            lists.append(l[j: i])
+    return [p for p in lists if len(p)==m]  
